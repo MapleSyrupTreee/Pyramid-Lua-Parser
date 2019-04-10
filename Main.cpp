9875b70fce919e8007e0802aea0584017e17c671
Samuel <49483389+SamuelFX@users.noreply.github.com>
2019-04-10 09:22:49 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -30,4 +30,15 @@ if (script.find("print") != -1) { // script containts print
 			}
 		}
 	}
+	if (script.find("warn") != -1) { // script containts print
+	if (script.find("(") != -1) { // script contains (
+		vector<string>printone = PyramidSplitString(script.c_str(), '('); // split (
+		if (printone.at(1).find('"') != -1) { // script containts "
+			vector<string>printtwo = PyramidSplitString(printone.at(1).c_str(), '"'); // split "
+		PyramidGetglobal(PyramidLua, "warn");
+		PyramidPushstring(PyramidLua, printtwo.at(1).c_str());
+		PyramidPCall(PyramidLua, 1, 0, 0);
+			}
+		}
+	}
 }
