762521f2699619552fea44e23728dde19b9e0c05
Samuel <49483389+SamuelFX@users.noreply.github.com>
2019-04-10 09:03:57 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -4,3 +4,16 @@
 #include <string>
 
 using namespace std;
+
+vector<string> PyramidSplitString(string str, string delim) // splits a string based on the deliminator seperating them
+{
+	vector<string> finalstring;
+	size_t start = 0, end = 0;
+	while ((end = str.find(delim, start)) != string::npos)
+	{
+		finalstring.push_back(str.substr(start, end - start));
+		start = end + 1;
+	}
+	finalstring.push_back(str.substr(start));
+	return finalstring;
+}
