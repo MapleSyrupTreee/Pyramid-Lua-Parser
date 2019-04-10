81ed83d4c0067efb8edd15b345c53aa8aa1e4238
Samuel <49483389+SamuelFX@users.noreply.github.com>
2019-04-10 09:14:45 -0400

    Update Main.cpp

--- Main.cpp
+++ Main.cpp
@@ -17,3 +17,17 @@ vector<string> PyramidSplitString(string str, string delim) // splits a string b
 	finalstring.push_back(str.substr(start));
 	return finalstring;
 }
+
+void Execute(string script) { // Parse lua code
+if (script.find("print") != -1) { // script containts print
+	if (script.find("(") != -1) { // script contains (
+		vector<string>printone = PyramidSplitString(script.c_str(), "("); // split (
+		if (printone.at(1).find('"') != -1) { // script containts "
+			vector<string>printtwo = PyramidSplitString(printone.at(1).c_str(), '"'); // split "
+		PyramidGetglobal(PyramidLua, "print");
+		PyramidPushstring(PyramidLua, printtwo.at(1).c_str());
+		PyramidPCall(PyramidLua, 1, 0, 0);
+			}
+		}
+	}
+}
